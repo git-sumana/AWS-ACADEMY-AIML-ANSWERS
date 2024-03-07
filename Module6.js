216d0d0c110f0f1a6cb70cb363d9058de3b5fcfc
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:34:20 +0530

    Create Module6.js

---
+++ Module6.js
@@ -0,0 +1,33 @@
+1. Why is AWS more economical than traditional data centers for applications with varying compute workloads
+Ans: Amazon EC2 instances can be launched on-demand when needed
+
+2. If your project requires monthly reports that iterate through very large amounts of data, which Amazon Elastic Cloud (Amazon EC2) purchasing option should you consider?
+Ans: Scheduled Reserved Instances
+
+3. What is included in an Amazon Machine Image (AMI)?
+Ans: All of the above
+
+4. Which Amazon Elastic Compute Cloud (Amazon EC2) feature ensures your instances will not share a physical host with instances from any other customer?
+Ans: Dedicated Instances
+
+5. Which of the following services is a serverless compute service in AWS?
+Ans: AWS Lambda
+
+6. What is the service provided by AWS that enables developers to easily deploy and manage applications in the cloud?
+Ans: AWS Elastic Beanstalk
+
+7. Your web application needs four instances to support study traffic all of the time. On the last day of the month, the traffic triples. What is the most cost-effective way to handle this pattern?
+Ans: Run for Reserved Instances constantly, then add eight On-Demand Instances on the last day of each month
+
+8. Containers contain an entire operating system
+True or False?
+False
+
+9. Which Amazon EC2 option is best for long-term workloads with predictable usage patterns?
+Ans: Reserved Instances
+
+10. Which of the following must be specified when launching a new Amazon Elastic Compute Cloud (Amazon EC2) Windows Instance
+(Choose 2)
+Ans: Amazon EC2 Instance type and Amazon Machine Image (AMI)
+
+
