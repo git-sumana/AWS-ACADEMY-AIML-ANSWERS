853de2113e292e4f1127b09f9dc9b27680ef9472
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:27:27 +0530

    Create Module2.js

---
+++ Module2.js
@@ -0,0 +1,139 @@
+Q1. Which Statement describes Machine Learning ? 
+
+A. The Scientific study of algorithms and statistical models to perform tasks by using inference instead  
+     of instructions. 
+
+B. The Creation of machines that can perform tasks that human would typically perform 
+
+C. The Compilation of a large set of rules that cover every Possible outcome. 
+
+D. A set of instruction that describe how to achieve a desired outcome. 
+
+Answer: [ A ]  The Scientific study of algorithms and statistical models to perform tasks by using                           
+                        inference instead of instructions. 
+
+
+Q2. Which type of training describes a machine learning application that interacts with its 
+       environment and learns to take actions that maximize rewards ? 
+
+A. Supervised Learning 
+
+B. Unsupervised Learning 
+
+C. Reinforcement Learning 
+
+D. Deep Learning 
+
+Answer:  [ C ]  Reinforcement Learning 
+
+
+Q3. You are Creating a machine learning solution for a call center. The objective of the system 
+       is to route customers to the appropriate department, and there are eight possible                            departments. Which type of Machine Learning Problem does this scenario describe ? 
+
+A. Regression 
+
+B. Multi class Classification 
+
+C. Binary Classification 
+
+D. Clustering 
+
+Answer: [ B ]  Multi class Classification 
+
+
+Q4. You are working on a machine learning problem that requires the system to respond 
+       to changes in the environment to improve performance. Which type of machine learning                 problem does this Scenario describe ? 
+ 
+A. Supervised Learning 
+
+B. Unsupervised Learning 
+
+C. Reinforcement Learning 
+
+D. Regression 
+
+Answer: [ D ]  Regression
+
+
+Q5. Which stage of the machine learning ( ML ) pipeline involves verifying that data is all of a 
+        uniform type ?
+
+A. Problem Formulation 
+
+B. Model Training 
+
+C. Data Preparation 
+
+D. Feature engineering 
+
+Answer: [ C ]  Data Preparation 
+
+
+Q6. You are working on a machine learning model that uses data from multiple countries. 
+       The countries are listed by using alphabetical abbreviations. Which stage involve                             converting these abbreviations to numerical Values ? 
+
+A. Data Preparation 
+
+B. Model tuning 
+
+C. Model Evaluation 
+
+D. Model Training 
+
+Answer: [ A ]  Data Preparation 
+
+
+Q7.  Your Model is overfitting if it performs well on the training data, but not on the 
+        evaluation data. 
+
+A. True 
+
+B. False 
+
+Answer: [ A ]  True 
+
+Q8. Which resource are Python libraries for working with machine learning problems ? 
+
+       ( Select TWO ) 
+
+
+A. Pandas 
+
+
+B. math 
+
+C. Scikit - Learn 
+
+D. Jupyter Notebooks 
+
+E. Bokeh 
+
+Answer: [ A ] , [ C ]  Pandas 
+ 
+                                   Scikt - learn 
+
+
+Q9. Which Amazon services can you use to deploy machine learning instances and run Jupyter             Notebooks ? 
+
+A. Amazon Comprehend 
+
+B. Amazon Sage Maker  
+
+C. Amazon Polly 
+
+D. Amazon Lex 
+
+Answer: [ B ]  Amazon Sage Maker 
+
+
+Q10. Which are requirements for choosing machine learning as a development methodology ? 
+
+A. A pre - defined machine learning model
+
+B. Large Data sets with a large number of variables 
+
+c. A complete set of rules for decision making 
+
+d. A stand alone data center  
+
+Answer: [ B ]  Large Data sets with a large number of variables 
