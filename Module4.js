60bde5f4b9e4ad8d8a1bbb11d5ada27672d1f67c
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:29:19 +0530

    Create Module4.js

---
+++ Module4.js
@@ -0,0 +1,128 @@
+AWS Academy Machine Learning Module 4 
+
+Q1. Which Patterns are common in the time series data ?
+
+A. Trends
+
+B. Seasonal
+
+C. Exponential
+
+D. Star Shaped
+
+E. None of the above
+
+Answer: [ A ], [ B ] Trends, Seasonal.
+
+Q2. Which use cases are apply to forecasting ? 
+
+A. Predicting the inventory that's required for items in a warehouse 
+
+B. Predicting if an X- ray image contains an abnormality 
+
+C. Predicting the energy consumption of an office 
+
+D. Predicting the Sentiment of a review
+
+E. Determining if two images are of the same person 
+
+Answer: [ A ] , [ C ]  
+
+Q3. Which datasets could be used as a time series data set? (Answer Two)
+
+A. Sales data that contains items, purchase dates and quantities
+
+B. Web logs that contain IP addresses, pages and timestamps
+
+C. Chemical Composition of food additives
+
+D. Membership data that contains personally identifiable information ( PII ) and a donate flag. 
+
+E. Results from a one-time survey. 
+
+Answer: [ A ] [ B ] 
+
+               Sales data that contains items, purchase dates and quantities
+
+               Web logs that contain IP addresses, pages and timestamps
+
+Q4. You have a dataset of temperature readings from a weather station. Temperature readings are logged for every 5 minutes. You notice that there are several missing values each day. Which approach do you follow. 
+
+A. Replace the missing values with zero 
+
+B. Forward fill the missing values. 
+
+C. Backward fill the missing values. 
+
+D. Use the sum of the temperatures for the day to fill the missing values. 
+
+E. Remove the records that have missing data. 
+
+Answer: [ B ] , [ C ] 
+
+Q5. Which scenarios are the examples of appropriate down sampling? 
+
+A.  Using mean to convert temperature readings every minute to an hourly value. 
+
+B. Using mean to convert sales information during the day to a daily total. 
+
+C. Using sum to convert sales order information during the day to daily total. 
+
+D. Using the sum to convert temperature readings every minute to tan hourly value. 
+
+Answer: [ A ] [ C ] 
+
+Q6. what examples of  seasonality that might you observe in the time series data ? 
+
+A. Quarterly, yearly.
+
+
+B. Spring, summer, fall, winter
+
+
+C. Every two years
+
+D. One time sales events
+
+E. Hourly.
+
+Answer: [ A ] [ B ] 
+
+Q7. Amazon forecast generates production for P10, P50 and P90. If the forecast predicts the shoe sales, what do the P10, P50 and P90 tell you. 
+
+
+A. P10 indicates 10% of the time, fewer than the predicted value will be ordered. 
+
+B. P50 indicates 50% of the time, the exact number of the predicted value will be ordered. 
+
+C. P90 indicates 90% of the time, more than the predicted value will be ordered.
+
+D. All the above. 
+
+Answer: [ A ]  P10 indicates 10% of the time, fewer than the predicted value will be ordered. 
+
+Q8. Which datasets are required for generating a retail forecast with Amazon Forecast? 
+
+A. Item data that includes an item and category. 
+
+B. Item stock information that includes a time stamp, item, and stock quantity. 
+
+C. Item pricing data including a timestamp item and price 
+
+D. Timeseries that include a timestamp , item and quantity 
+
+Answer: [ D ] 
+
+Q9. You are going to use a dataset, to generate a forecast. Which steps would you take to use the data that's available to produce the best model? 
+
+A. Use the train_test_split_function from scikit-learn to create a training and testing dataset. 
+
+B. Use pandas to split the data by time into a training and testing dataset. 
+
+C. Use the training data set in Amazon forecast by specifying a backset window. 
+
+D. Using the testing dataset to compare the predicted values with actual values. 
+
+E. All the Above
+
+Answer: [ B ] [ C ] [ D ] 
