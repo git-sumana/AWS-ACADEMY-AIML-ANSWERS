127078abc4f105a6680e06cdefd8f279afd52d45
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:28:19 +0530

    Create Module3.js

---
+++ Module3.js
@@ -0,0 +1,140 @@
+AWS Academy Machine Learning Module 3 
+
+Q1. Which resources help define a machine learning ? (SELECT TWO)
+A. Access to labeled data 
+
+B. A domain expert to consult 
+
+C. A traditional coded solution 
+
+D. Sufficient Hardware 
+
+E. A neural network 
+
+Answer: [ A] , [ B ] 
+
+               Access to labeled data 
+
+               A domain expert to consult 
+
+Q2. When preparing data for supervised classification machine learning, which attributes should 
+
+       the data have? (SELECT TWO)
+
+A. Data should be labeled 
+
+B. Data should contain only instances of the target 
+
+C. Anyone in the company should be able to access the data 
+
+D. Data should be generated randomly by using genetic algorithms
+
+E. Data should be representative of production 
+
+Answer: [ A ] , [ E ] 
+
+               Data should be labeled 
+
+               Data should be representative of production 
+
+Q3. What can you learn by examining the statistics of your data ? 
+
+A. Identifying anamolies in the data 
+
+B. Verifying that the data is formatted correctly 
+
+C. Removing outliers 
+
+D. Filling in missing Data 
+
+Answer: [ A ]  Identifying anamolies in the data 
+
+Q4. You have preprocessed dataset that's react for use in training a model. How should 
+
+       you divide your training data ? 
+
+A. Use all the data to train the model 
+
+B. Split the data into two equal sets. Use one half for training and the other half for the testing. 
+
+C. Split the data into three sets. Use 80% for training, 10% for testing and 10% for validation.
+
+D. Split the data into two sets. Use 80% for training and 20% for testing and validation. 
+
+Answer: [ C ] 
+
+  Split the data into three sets. Use 80% for training, 10% for testing and 10% for validation. 
+
+Q5.You can select between single model and multi - model hosting with Amazon Sage Maker. 
+
+A. True 
+
+B. False 
+
+Answer: [ A ]  True 
+
+Q6. What is the Purpose of a confusion matrix ? 
+
+A. To plot the labels from the Predicts dataset. 
+
+B. To show the true or false positives, along with the true or false negatives
+
+C. To show the correlation between two columns in the dataset.
+
+D. To stratly the Classes across training and testing data sets.
+
+Answer: [ B ]  To show the true or false positives, along with the true or false negatives
+
+Q7. What does the correlation heatmap show?
+
+A. The level of correlation between features in a dataset 
+
+B. The level of correlation between the test and the validation data 
+
+C. The level of correlation between the predicted and the actual values
+
+D. The level of correlation between encoded and text data 
+
+Answer: [ A ]  The level of correlation between features in a dataset 
+
+Q8. Which of the following file formats does pandas support for data importing ? 
+
+       ( SELECT TWO ) 
+
+A. JSON 
+
+B. MS Word 
+
+C. CSV 
+
+D. Binary files 
+
+E. PDF 
+
+Answer: [ A] , [ C ]   JSON , CSV 
+
+Q9. Which Amazon service can you use to deploy machine learning instances and run 
+
+       Jupyter Notebooks ? 
+
+A. Amazon Comprehend 
+
+B. Amazon Sage Maker 
+
+C. Amazon Polly 
+
+D. Amazon Lex 
+
+Answer: [ B ]  Amazon Sage Maker 
+
+Q10. What is the goal of an Amazon Sage Maker hyperparameter tuning job ? 
+
+A. To optimize metrics for training. 
+
+B. To optimize the model parameters to produce the best model 
+
+C. To optimize the data inputs to produce the fastest prediction 
+
+D. To optimize the algorithm choice to produce the best model. 
+
+Answer: [ B ] To optimize the model parameters to produce the best model 
